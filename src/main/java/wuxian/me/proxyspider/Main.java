4da26da530fd07d0846967187956c3bc6134c20c
xiaoshenke <1205914137@qq.com>
2017-07-01 01:06:17 +0800

    -

--- src/main/java/wuxian/me/proxyspider/Main.java
+++ src/main/java/wuxian/me/proxyspider/Main.java
@@ -1,8 +1,11 @@
 package wuxian.me.proxyspider;
 
 import wuxian.me.proxyspider.xun.XunProxySpider;
+import wuxian.me.spidermaster.FakeProxyProvider;
+import wuxian.me.spidermaster.biz.agent.provider.ProviderScan;
 import wuxian.me.spidersdk.IJobManager;
 import wuxian.me.spidersdk.JobManagerConfig;
+import wuxian.me.spidersdk.anti.Fail;
 import wuxian.me.spidersdk.job.IJob;
 import wuxian.me.spidersdk.job.JobProvider;
 import wuxian.me.spidersdk.manager.JobManagerFactory;
@@ -14,6 +17,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ProviderScan.performCheckAndCollect(ProxyProvider.class);
+
         JobManagerConfig.init();
 
         IJobManager jobManager = JobManagerFactory.getJobManager();
@@ -22,5 +27,7 @@ public class Main {
         IJob job = JobProvider.getJob();
         job.setRealRunnable(new XunProxySpider());
         //jobManager.putJob(job);
+
+        //jobManager.fail(new XunProxySpider(), Fail.BLOCK); //用于模拟被封 会执行切换proxy
     }
 }
--- src/main/java/wuxian/me/proxyspider/ProxyProvider.java
+++ src/main/java/wuxian/me/proxyspider/ProxyProvider.java
@@ -2,26 +2,30 @@ package wuxian.me.proxyspider;
 
 import wuxian.me.proxyspider.xun.XunData;
 import wuxian.me.proxyspider.xun.XunProxyPool;
+import wuxian.me.spidercommon.log.LogManager;
 import wuxian.me.spidercommon.model.Proxy;
-import wuxian.me.spidermaster.framework.master.provider.IProvider;
-import wuxian.me.spidermaster.framework.master.provider.Provider;
+import wuxian.me.spidermaster.biz.provider.IProvider;
+import wuxian.me.spidermaster.biz.provider.Provider;
+import wuxian.me.spidermaster.framework.common.GsonProvider;
 
 /**
  * Created by wuxian on 20/6/2017.
  */
 
 @Provider(provide = "proxy")
-public class ProxyProvider implements IProvider<Proxy> {
+public class ProxyProvider implements IProvider<String> {
 
     public ProxyProvider() {
     }
 
-    public Proxy provide() {
+    //Fixme:现在的序列化能力太弱了 返回只能是string....
+    public String provide() {
+        LogManager.info("ProxyProvider.provide");
         XunData xunData = XunProxyPool.getXunProxy();
         if (xunData == null) {
             return null;
         }
 
-        return new Proxy(xunData.ip, Integer.parseInt(xunData.port));
+        return GsonProvider.gson().toJson(new Proxy(xunData.ip, Integer.parseInt(xunData.port)));
     }
 }
